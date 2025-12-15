8f0ec969c4b3192be4f6e713621be50f949c473b
kovaielelarasanc <147711761+kovaielelarasanc@users.noreply.github.com>
2025-12-15 12:42:06 +0530

    token config

--- app/core/config.py
+++ app/core/config.py
@@ -57,7 +57,7 @@ class Settings(BaseModel):
     JWT_SECRET: str = os.getenv("JWT_SECRET", "change-this")
     JWT_ALG: str = os.getenv("JWT_ALG", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
-        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "2440"))
     REFRESH_TOKEN_EXPIRE_MINUTES: int = int(
         os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", str(7 * 24 * 60)))
     BCRYPT_ROUNDS: int = int(os.getenv("BCRYPT_ROUNDS", "12"))
